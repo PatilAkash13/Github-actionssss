048fee6390ce241111e49f5a630cd0ea8e6b9a1a
Akash <patilakash463@gmail.com>
2024-12-19 14:06:50 +0000

    deployy

--- HttpTrigger2.cs
+++ HttpTrigger2.cs
@@ -18,7 +18,7 @@ namespace Company.Function
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("action github!");
+            return new OkObjectResult("123456789!");
         }
     }
 }
